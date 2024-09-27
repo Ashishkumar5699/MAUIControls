cd03bdad60cc397a88ac67e2c2ec62d42beb0239
Ashish Kumar <Ashishsarraf7@gmail.com>
2024-09-27 17:39:51 +0530

    Added: Conditional Expander, Signature pad, BooltoColor converter

---
+++ CustomControlFramework/Converter/BoolToColorConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace CustomControlFramework.Converter
+{
+	public class BoolToColorConverter : IValueConverter, IMarkupExtension
+    {
+		public BoolToColorConverter()
+		{
+		}
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var result = false;
+
+            if(value is bool _condition)
+            {
+                result = _condition;
+            }
+
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ProvideValue(IServiceProvider serviceProvider) => this;
+    }
+}
+
---
+++ CustomControlFramework/Entry/SignaturePad.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace CustomControlFramework.Entry;
+
+public partial class SignaturePad : ContentView
+{
+	public SignaturePad()
+	{
+		InitializeComponent();
+	}
+
+    void Button_Clicked(System.Object sender, System.EventArgs e)
+    {
+        DrawBoard.Lines.Clear();
+    }
+
+    private void DrawBoard_DrawingLineCompleted(System.Object sender, CommunityToolkit.Maui.Core.DrawingLineCompletedEventArgs e)
+    {
+        ImageView.Dispatcher.Dispatch(async () =>
+        {
+            var stream = await DrawBoard.GetImageStream(300, 300);
+            ImageView.Source = ImageSource.FromStream(() => stream);
+        });
+    }
+}
