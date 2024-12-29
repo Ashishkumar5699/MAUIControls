40d271ba2f5c47d9d00377ed170c717fd652681b
Ashish <Ashishsarraf7@gmail.com>
2024-12-29 12:17:36 +0530

    added Empty BaseViewModel

---
+++ CustomControlFramework/Page/ContentPageMobileBaseViewModel.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace CustomControlFramework.Page;
+
+public class ContentPageMobileBaseViewModel
+{
+    // [ObservableProperty]
+    // string title;
+
+    // [ObservableProperty]
+    // bool isBusy;
+
+    // [ObservableProperty]
+    // double progress;
+}
